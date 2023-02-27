679d9687521d8673745448c29dad6c5c7f8f4bc3
Michael Anderson <anderson@mbari.org>
2023-02-27 15:26:58 -0800

    add comments about pack rates
    Signed-off-by: Michael Anderson <anderson@mbari.org>

--- mbari_wec_template_py/controller.py
+++ mbari_wec_template_py/controller.py
@@ -49,9 +49,11 @@ class Controller(Interface):
         self.policy = ControlPolicy()
         self.set_params()
 
-        # set any pack rates here
-        # self.set_pc_pack_rate_param()
-        # self.set_sc_pack_rate_param()
+        # set packet rates from controllers here
+        # controller defaults to publishing @ 10Hz
+        # call these to set rate to 50Hz or provide argument for specific rate
+        # self.set_pc_pack_rate_param()  # set SC publish rate to 50Hz
+        # self.set_sc_pack_rate_param()  # set PC publish rate to 50Hz
 
     # To subscribe to any topic, simply define the specific callback, e.g. power_callback
     # def power_callback(self, data):
